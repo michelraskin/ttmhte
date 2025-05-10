d0e42d97b524dd31dfed639fa3e43c899777ed7e
Michel Raskin <mbranda1@jh.edu>
2025-05-10 14:15:56 -0400

    adding mimic folder

---
+++ mimiciv/PMAPUtil.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+# death_at_disch hypothermia 
+def getTrainTestFunctions(aPredictedColumn = 'LastMGCSPositive', aTreatmentColumn = 'hypothermia', aTestSize = 0.3, aTreatmentSplit = False, aDropColumns = [], aSkipTemp = True):
+    myPredictorsDf = pd.read_csv('PMAP_Predictors2.csv')
+
+    top_corr = pd.read_csv('top_correlations.csv')
+    columns_to_drop = top_corr[top_corr['hypothermia'].abs() > 0.7]['Unnamed: 0'].tolist()
+    
+    # Preprocessing
+    myFilter = (myPredictorsDf['first_mGCS_time'] != myPredictorsDf['last_mGCS_time']) 
+    myPredictorsDf.loc[myPredictorsDf['death_at_disch'] == 1, 'last_mGCS'] = 1
+    myPredictorsDf.loc[myFilter, 'LastMGCSPositive'] = (myPredictorsDf.loc[myFilter, 'last_mGCS'].astype(float) == 6).astype(int)
+
+    myBinaryColumns = myPredictorsDf.columns[myPredictorsDf.nunique() == 2] 
+    myBinaryDf = myPredictorsDf[myBinaryColumns]
+    myBinaryDf = myBinaryDf.select_dtypes(exclude=["object"])
+    myLowColumns = myBinaryDf.columns[(myBinaryDf.sum() < 15)]
+    myPredictorsDf.drop(columns=list(myLowColumns) + aDropColumns, inplace=True)
+
+    myGcs15Fitler = (myPredictorsDf.first_mGCS != 6)
+
+    if (aPredictedColumn == 'LastMGCSPositive'):
+        myPredictorsDf = myPredictorsDf[myGcs15Fitler & myFilter & ~myPredictorsDf[aTreatmentColumn].isna()]
+    else:
+        myPredictorsDf = myPredictorsDf[myGcs15Fitler & ~myPredictorsDf[aPredictedColumn].isna() & ~myPredictorsDf[aTreatmentColumn].isna()]
+
+    myColumns = []
+    if (aSkipTemp):
+        myColumns = [x for x in myPredictorsDf.columns if 'emp' in x]
+
+    # Get output data
+    myXValue = myPredictorsDf.drop(columns=  myColumns + ['first_mGCS', 'last_mGCS_time', 'first_mGCS_time', 'LastMGCSPositive', 'last_mGCS', 'death_at_disch'])
+    myXValue = myXValue
+    myXValue = myXValue.select_dtypes(exclude=['object'])
+    myYValue = myPredictorsDf[aPredictedColumn]
+    myYValue = myYValue.astype(int)
+
+    if (aTreatmentSplit):
+        myXValue = myXValue.drop(columns=[aTreatmentColumn])
+        X_train, X_test, T_train, T_test, y_train, y_test = train_test_split(myXValue, myPredictorsDf[aTreatmentColumn], myYValue, stratify=myPredictorsDf[[aPredictedColumn, aTreatmentColumn]], test_size=aTestSize)
+        return myPredictorsDf, X_train, X_test, T_train, T_test, y_train, y_test
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(myXValue, myYValue, stratify=myPredictorsDf[[aPredictedColumn, aTreatmentColumn]], test_size=aTestSize)
+        return myPredictorsDf, X_train, X_test, y_train, y_test
