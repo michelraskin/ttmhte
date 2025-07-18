e7f0faf0c30374dd841cf16dd1fc534b403ce3dc
Michel Raskin <mbranda1@jh.edu>
2025-07-18 19:50:59 -0400

    new changes

--- mimiciv/MIMICUtil.py
+++ mimiciv/MIMICUtil.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 # death_at_disch hypothermia 
 def getTrainTestFunctions(aPredictedColumn = 'LastMGCSPositive', aTreatmentColumn = 'hypothermia', aTestSize = 0.3, aTreatmentSplit = False, aDropColumns = [], aSkipTemp = True):
-    myPredictorsDf = pd.read_csv('MIMIC_Predictors.csv')
+    myPredictorsDf = pd.read_csv('MIMIC_Predictors1.csv')
 
     top_corr = pd.read_csv('top_correlations.csv')
     columns_to_drop = top_corr[top_corr['hypothermia'].abs() > 0.7]['Unnamed: 0'].tolist()
@@ -31,7 +31,7 @@ def getTrainTestFunctions(aPredictedColumn = 'LastMGCSPositive', aTreatmentColum
         myColumns = [x for x in myPredictorsDf.columns if 'emp' in x or 'dx_' == x[0:3]]
 
     # Get output data
-    myXValue = myPredictorsDf.drop(columns=columns_to_drop +  myColumns + ['first_mGCS', 'last_mGCS_time', 'first_mGCS_time', 'LastMGCSPositive', 'last_mGCS', 'death_at_disch', 'first_mGCS_time.1', 'first_mGCS.1', 'last_mGCS_time.1', 'last_mGCS.1'])
+    myXValue = myPredictorsDf.drop(columns=columns_to_drop +  myColumns + ['first_mGCS', 'last_mGCS_time', 'first_mGCS_time', 'LastMGCSPositive', 'last_mGCS', 'death_at_disch'])
     myXValue = myXValue
     myXValue = myXValue.select_dtypes(exclude=['object'])
     myYValue = myPredictorsDf[aPredictedColumn]
